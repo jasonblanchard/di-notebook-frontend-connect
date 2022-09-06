c7051e531178bae3290b81781644125a7530d775
jasonblanchard <jas.blanchard@gmail.com>
2022-09-05 20:15:19 -0400

    sidebar buttons

--- components/layout/SidebarLayout.tsx
+++ components/layout/SidebarLayout.tsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react';
 import useHotkeys from '@reecelucas/react-use-hotkeys';
 import React from 'react';
-import { ChevronLeftIcon } from '@heroicons/react/24/solid';
+import { ChevronLeftIcon, PlusCircleIcon } from '@heroicons/react/24/outline';
 
 interface SidebarLayoutProps {
     children: React.ReactNode
@@ -33,9 +33,20 @@ export default function SidebarLayout({ children, nav }: SidebarLayoutProps) {
             <div className="h-2 bg-gradient-to-r to-primary-600 from-primary-300" />
             <div className="flex h-full">
                 <div className={`${isCollapsed ? 'w-0' : 'w-40'} transition-all duration-100 border-r bg-neutral-600 relative`}>
-                    <button onClick={toggleCollapsed} className="absolute py-1 w-6 bg-neutral-600 -right-6 top-3 text-neutral-300 rounded-r-sm">
-                        <ChevronLeftIcon className={`${isCollapsed ? 'rotate-180' : ''} transition-transform`} />
-                    </button>
+                    <div className="flex flex-col justify-center align-middle absolute py-2 w-8 bg-neutral-600 -right-8 top-3 text-neutral-300 rounded-r-sm">
+                        <button onClick={toggleCollapsed} className="flex align-middle justify-center py-2 hover:bg-neutral-700">
+                            <span className="sr-only">collapse sidebar</span>
+                            <ChevronLeftIcon className={`h-6 w-6 ${isCollapsed ? 'rotate-180' : ''} transition-transform`} />
+                        </button>
+                        <button className="flex align-middle justify-center py-2 hover:bg-neutral-700">
+                            <span className="sr-only">new entry</span>
+                            <PlusCircleIcon className="h-6 w-6" />
+                        </button>
+                        <button className="flex align-middle justify-center py-2 hover:bg-neutral-700">
+                            <span className="sr-only">keyboard shortcuts</span>
+                            ⌘   
+                        </button>
+                    </div>
                     {!isCollapsed && (
                         <nav className="text-sm text-neutral-300">
                             {nav}
