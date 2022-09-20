a1f598bc1a071091d76ab08240ec98dd4ac0b044
jasonblanchard <jas.blanchard@gmail.com>
2022-09-19 22:22:51 -0400

    add preview

--- components/EntryEditor.tsx
+++ components/EntryEditor.tsx
@@ -1,6 +1,8 @@
 import { useState, useEffect, useMemo } from 'react';
 import debounce from 'lodash.debounce';
 import { ExclamationTriangleIcon, TrashIcon } from '@heroicons/react/24/outline'
+import ReactMarkdown from 'react-markdown';
+import remarkGfm from 'remark-gfm';
 
 import { button } from 'styles/classnames';
 import useWriteToEntry from 'queries/useWriteToEntry';
@@ -65,65 +67,70 @@ export default function EntryEditor({ initialText, id, createdAt, updatedAt }: E
     }
 
     return (
-        <div className="flex py-12 h-full flex-col lg:max-w-3xl mx-auto">
-            <div className="flex mb-2 justify-between text-sm text-neutral-400">
-                <div>
-                    {!writeToEntry.isError && (
-                        writeToEntry.isLoading ? 
-                            <div className="flex items-center"><div className="rounded-full bg-yellow-400 h-3 w-3 mr-1" />Saving...</div>
-                            : <div className="flex items-center"><div className="rounded-full bg-green-400 h-3 w-3 mr-1" />Saved</div>
-                        )
-                    }
-                    {writeToEntry.isError && <div className="flex items-center"><div className="rounded-full bg-red-400 h-3 w-3 mr-1" />Error</div>}
-                </div>
-                <div>
-                    {`${createdAt.toLocaleString()}${updatedAt ? ` • ${updatedAt.toLocaleString()}` : ""}`}
-                </div>
-            </div>
-            <div className="w-full flex-1 relative">
-                <textarea
-                    autoFocus
-                    className="w-full text-lg absolute top-0 bottom-0 bg-neutral-100 border resize-none border-neutral-200 rounded-0 p-4 shadow text-neutral-700 leading-relaxed border-none focus:ring-2 focus:ring-primary-400 placeholder:text-neutral-300"
-                    value={text}
-                    onChange={handleChange}
-                />
-            </div>
-            <div className="flex justify-end py-2">
-                <button className={button.tertiary} onClick={() => setIsDeleteModalOpen(true)}>Delete</button>
-            </div>
-            <Modal isOpen={isDeleteModalOpen} onRequestClose={() => setIsDeleteModalOpen(false)}>
-                <div className="sm:flex sm:items-start">
-                    <div className="mx-auto flex h-12 w-12 flex-shrink-0 items-center justify-center rounded-full bg-danger-red-100 sm:mx-0 sm:h-10 sm:w-10">
-                        <ExclamationTriangleIcon className="h-6 w-6 text-danger-red-600" aria-hidden="true" />
+        <div className="flex justify-around py-12 h-full mx-auto">
+            <div className="w-full sm:w-1/2 sm:mx-6">
+                <div className="flex mb-2 justify-between text-sm text-neutral-400">
+                    <div>
+                        {!writeToEntry.isError && (
+                            writeToEntry.isLoading ? 
+                                <div className="flex items-center"><div className="rounded-full bg-yellow-400 h-3 w-3 mr-1" />Saving...</div>
+                                : <div className="flex items-center"><div className="rounded-full bg-green-400 h-3 w-3 mr-1" />Saved</div>
+                            )
+                        }
+                        {writeToEntry.isError && <div className="flex items-center"><div className="rounded-full bg-red-400 h-3 w-3 mr-1" />Error</div>}
                     </div>
-                    <div className="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
-                        <ModalTitle as="h3" className="text-lg font-medium leading-6 text-gray-900">
-                            Delete Entry
-                        </ModalTitle>
-                        <div className="mt-2">
-                            <p className="text-sm text-gray-500 py-4">
-                                Are you sure?
-                            </p>
+                    <div>
+                        {`${createdAt.toLocaleString()}${updatedAt ? ` • ${updatedAt.toLocaleString()}` : ""}`}
+                    </div>
+                </div>
+                <div className="w-full h-full flex-1 relative">
+                    <textarea
+                        autoFocus
+                        className="w-full text-lg absolute top-0 bottom-0 bg-neutral-100 border resize-none border-neutral-200 rounded-0 p-4 shadow text-neutral-700 leading-relaxed border-none focus:ring-2 focus:ring-primary-400 placeholder:text-neutral-300"
+                        value={text}
+                        onChange={handleChange}
+                    />
+                </div>
+                <div className="flex justify-end py-2">
+                    <button className={button.tertiary} onClick={() => setIsDeleteModalOpen(true)}>Delete</button>
+                </div>
+                <Modal isOpen={isDeleteModalOpen} onRequestClose={() => setIsDeleteModalOpen(false)}>
+                    <div className="sm:flex sm:items-start">
+                        <div className="mx-auto flex h-12 w-12 flex-shrink-0 items-center justify-center rounded-full bg-danger-red-100 sm:mx-0 sm:h-10 sm:w-10">
+                            <ExclamationTriangleIcon className="h-6 w-6 text-danger-red-600" aria-hidden="true" />
+                        </div>
+                        <div className="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
+                            <ModalTitle as="h3" className="text-lg font-medium leading-6 text-gray-900">
+                                Delete Entry
+                            </ModalTitle>
+                            <div className="mt-2">
+                                <p className="text-sm text-gray-500 py-4">
+                                    Are you sure?
+                                </p>
+                            </div>
                         </div>
                     </div>
-                </div>
-                <div className="mt-5 sm:mt-4 sm:flex sm:flex-row-reverse">
-                    <button
-                        type="button"
-                        className={`${button.danger} w-full mb-2 sm:mb-0 sm:ml-4`}
-                        onClick={() => deleteEntry.mutate()}
-                    >
-                        Yes, delete
-                    </button>
-                    <button
-                        type="button"
-                        className={`${button.secondary} w-full`}
-                        onClick={() => setIsDeleteModalOpen(false)}
-                    >
-                        Nevermind
-                    </button>
-                </div>
-            </Modal>
+                    <div className="mt-5 sm:mt-4 sm:flex sm:flex-row-reverse">
+                        <button
+                            type="button"
+                            className={`${button.danger} w-full mb-2 sm:mb-0 sm:ml-4`}
+                            onClick={() => deleteEntry.mutate()}
+                        >
+                            Yes, delete
+                        </button>
+                        <button
+                            type="button"
+                            className={`${button.secondary} w-full`}
+                            onClick={() => setIsDeleteModalOpen(false)}
+                        >
+                            Nevermind
+                        </button>
+                    </div>
+                </Modal>
+            </div>
+            <div className="hidden sm:block w-1/2 mx-6 mt-5 overflow-scroll h-full prose">
+                <ReactMarkdown remarkPlugins={[remarkGfm]}>{text}</ReactMarkdown>
+            </div>
         </div>
     )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -67,5 +67,6 @@ module.exports = {
   plugins: [
     require('@tailwindcss/forms'),
     require('@tailwindcss/line-clamp'),
+    require('@tailwindcss/typography'),
   ],
 }
